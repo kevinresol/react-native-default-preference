611fb37223cee9b5efec52b8d7f9a93468a31a6d
Evan Gillogley <evanjmg@gmail.com>
2019-03-31 22:35:25 +0100

    add android polyfill

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 
-import { NativeModules } from 'react-native';
+import { NativeModules, Platform } from 'react-native';
 
 const { RNDefaultPreference } = NativeModules;
+let DefaultPreference = RNDefaultPreference;
 
-export default RNDefaultPreference;
+if (Platform.OS !== 'ios') {
+  
+  const setObject = (key, obj) => {
+    RNDefaultPreference.set(key, JSON.stringify(obj));
+  }
+  const getObject = async (key) => {
+    const stringifiedObject = await RNDefaultPreference.get(key);
+    if (stringifiedObject) {
+      return JSON.parse(stringifiedObject);
+    }
+    return null
+  }
+  DefaultPreference = {
+    ...RNDefaultPreference,
+    setObject,
+    getObject,
+    getArray: getObject,
+    setArray: setObject, 
+  }
+} 
+export default DefaultPreference;
