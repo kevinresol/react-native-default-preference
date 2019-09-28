ac5479ea9be7344be16514216368cc544f10a4d4
Marcel Kalveram <marcel.kalveram@googlemail.com>
2019-09-28 11:32:35 +0200

    Clean up header file

--- ios/RNDefaultPreference.h
+++ ios/RNDefaultPreference.h
@@ -1,14 +1,7 @@
 
 // import RCTBridgeModule
-#if __has_include(<React/RCTBridgeModule.h>)
 #import <React/RCTBridgeModule.h>
-#elif __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#else
-#import "React/RCTBridgeModule.h" // Required when used as a Pod in a Swift project
-#endif
 
 @interface RNDefaultPreference : NSObject <RCTBridgeModule>
 
 @end
-  
