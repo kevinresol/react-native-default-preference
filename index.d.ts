e7391266172f7d4123966989bdf8090ffd1679b2
Alan Keiss <alan.keiss@gmail.com>
2019-10-25 11:21:46 +0200

    Object -> { [key: string]: string }

--- index.d.ts
+++ index.d.ts
@@ -4,9 +4,9 @@ declare module 'react-native-default-preference' {
         static set(key: String, value: String): Promise<void>;
         static clear(key: String): Promise<void>;
         static getMultiple(keys: Array<String>): Promise<Array<String>>;
-        static setMultiple(data: Object): Promise<void>;
+        static setMultiple(data: { [key: string]: string }): Promise<void>;
         static clearMultiple(keys: Array<String>): Promise<void>;
-        static getAll(): Promise<Object>;
+        static getAll(): Promise<{ [key: string]: string }>;
         static clearAll(): Promise<void>;
 
         /** Gets and sets the current preferences file name (android) or user default suite name (ios) **/
