1d3415c325469946076f3e1350f0d7dec2b13f6a
Alan Keiss <alan.keiss@gmail.com>
2019-10-25 10:48:50 +0200

    add TypeScript index.d.ts file

---
+++ index.d.ts
@@ -0,0 +1,16 @@
+declare module 'react-native-default-preference' {
+    export default class DefaultPreference {
+        static get(key: String): Promise<String>;
+        static set(key: String, value: String): Promise<void>;
+        static clear(key: String): Promise<void>;
+        static getMultiple(keys: Array<String>): Promise<Array<String>>;
+        static setMultiple(data: Object): Promise<void>;
+        static clearMultiple(keys: Array<String>): Promise<void>;
+        static getAll(): Promise<Object>;
+        static clearAll(): Promise<void>;
+
+        /** Gets and sets the current preferences file name (android) or user default suite name (ios) **/
+        static getName(): Promise<String>;
+        static setName(name: String): Promise<void>;
+    }
+}
